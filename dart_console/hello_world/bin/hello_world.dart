f160b82a5dabbdbccf8f6608e815ffcc4a966bc6
Agustin Jose <agustinwawrzyk@protonmail.com>
2021-08-06 14:48:09 -0300

    variables

--- dart_console/hello_world/bin/hello_world.dart
+++ dart_console/hello_world/bin/hello_world.dart
@@ -1,3 +1,25 @@
 void main(List<String> arguments) {
   print('Hello world');
+
+  int numero;
+  numero = 0;
+
+  int numero1;
+  numero1 = 1;
+
+  print(numero);
+  print(numero1);
+
+  double numero2;
+  numero2 = 9.0;
+  print(numero2);
+
+  //texto
+
+  String texto = 'Mis numeros \n$numero  \n$numero2 \n$numero1';
+  print(texto);
+
+  //bool
+
+  bool interrupcion = true;
 }
