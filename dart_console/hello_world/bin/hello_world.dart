072e9797c0e87c50c0fa64d6a173a6ce9e39a571
Agustin Jose <agustinwawrzyk@protonmail.com>
2021-08-17 17:07:32 -0300

    funciones

--- dart_console/hello_world/bin/hello_world.dart
+++ dart_console/hello_world/bin/hello_world.dart
@@ -1,68 +1,30 @@
-//import 'dart:convert';
-//import 'dart:io';
-
 void main(List<String> arguments) {
-  /* int vidaJugador = 100;
-  final ataqueEnemigo = 45;
+  var esDinero = tengoDinerillo(-1000.5);
+  print(esDinero);
 
-  vidaJugador -= ataqueEnemigo;
-  vidaJugador -= ataqueEnemigo;
-  vidaJugador -= ataqueEnemigo;
-  // sentencias de control de flujo if-else
-  if (vidaJugador <= 0) {
-    print("Muerto");
-  } else
-    print("Vivo"); */
+  tengoDinerillo_SR(-1000.5);
+}
 
-  //causistica con else if
-/*   final int vidaInicial = 100;
-  int vidaJugadorActual = 100;
+/*
+ tipo_a_devolver nombreDeLaFuncion(params){
+   return o no;
+ }
+ */
+//funcion sin return y con parametros de entrada
+void tengoDinerillo_SR(double dinerilloCuenta) {
+  print(dinerilloCuenta > 0);
+}
+//funcion con return y con parametros de entrada
+bool tengoDinerillo(double dinerilloCuenta) {
+  return (dinerilloCuenta > 0);
+}
 
-  final ataqueEnemigo = 50;
+//funcion sin return y sin parametros de entrada
+void tengoDinerillo_SR_SE() {
+  print(-1500.5 > 0);
+}
 
-  vidaJugadorActual -= ataqueEnemigo;
-  if (vidaJugadorActual <= 0) {
-    print("muerto");
-  } else if (vidaJugadorActual < vidaInicial && vidaJugadorActual > 0) {
-    print("esta herido $vidaJugadorActual");
-  } else {
-    print("esta vivo");
-  } */
-
-  // switch
-  print('------------------------------------------');
-  //var productoElegido = stdin.readLineSync();
-  var productoElegido = 'hamburguesa doble';
-
-  switch (productoElegido.trim()) {
-    case 'hamburguesa doble':
-      print('enviando el producto elegido a cocina');
-      break;
-    case 'hamburguesa simple':
-      print('enviando el producto elegido a cocina');
-      break;
-    default:
-      print('repite el pedido');
-      break;
-  }
-
-  // bucle for
-  print('------------------------------------------');
-  var listaDeCompra = ['galletas', 'pepas', 'ardillas'];
-  for (var productoDeLaLista in listaDeCompra) {
-    if (productoDeLaLista == 'pepas') break;
-    print('** - > $productoDeLaLista');
-  }
-
-  // while
-
-  // assert
-
-  var texto = 'assert aplicado';
-  assert(texto != null);
-
-  // expression ?
-  // condicion ? true : false;
-  (1 == 1) ? print('true') : print('false');
-  (1 == 2) ? print('true') : print('false');
+//funcion con return y sin parametros de entrada
+bool tengoDinerillo_CR_SP() {
+  return (-1500.5 > 0);
 }
