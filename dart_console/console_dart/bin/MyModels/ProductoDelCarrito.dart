e264a6745c4398fe56319bca87d1c3e169499f90
Agustin Jose <agustinwawrzyk@protonmail.com>
2021-08-17 17:38:56 -0300

    clases

---
+++ dart_console/console_dart/bin/MyModels/ProductoDelCarrito.dart
@@ -0,0 +1,16 @@
+class ProductoDelCarrito {
+  // parametros o variables internas
+  String nombre = '';
+  String descripcion = '';
+  double precio = 0;
+
+  //constructor (productoDelCarrito1)
+  //ProductoDelCarrito();
+
+  //constructor con parametros(productoDelCarrito2)
+  ProductoDelCarrito(String nombre, String descripcion, double precio) {
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precio = precio;
+  }
+}
